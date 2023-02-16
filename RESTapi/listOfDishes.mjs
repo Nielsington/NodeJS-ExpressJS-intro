694f41d2bbccae105e9f40e7ce1046f9f8924030
Nielsington <declercqniels@hotmail.com>
2023-02-16 12:38:32 +0100

    Add list of dishes

---
+++ RESTapi/listOfDishes.mjs
@@ -0,0 +1,84 @@
+export const dishes = [
+    {
+        name: "Wonton soup with rabbit",
+        prepTime: "60 minutes",
+        ingredients: [
+            "16 wonton sheets",
+            "3l water",
+            "a few lime leaves",
+            "4 stalks of celery",
+            "1 stalk of leek",
+            "2 onions",
+            "2 cloves of garlic",
+            "60g of ginger",
+            "2 stalks of lemongrass",
+            "half a chili pepper",
+            "big dash of fish sauce",
+            "half a Chinese cabbage",
+            "2 rabbit legs",
+            "200 g mini shiitakes",
+            "100 g soy sprouts",
+            "100 g slumber peas",
+            "half a red chili pepper",
+            "a dash of sesame oil",
+            "a dash of soy sauce",
+            "2 deciliters coconut milk",
+            "2 stalks of spring onions"
+            ],
+        steps:[
+            "Remove the wonton sheets from the freezer in a timely manner and allow them to thaw.",
+            "Bring the water to a boil and add the lime leaves. Add the vegetables and seasonings to the broth: celery, leeks, onions, garlic, chili pepper, lemongrass, ginger and finally the fish sauce. Everything may be coarsely chopped.",
+            "Finally, finely chop the Chinese cabbage and add its hard core to the broth. Keep the finely chopped strips aside for a while.",
+            "Place the rabbit legs in the broth and bring to a boil. Simmer the broth for 45 minutes to an hour, until the legs are cooked and tender.",
+            "Put the mini shiitake and soy shoots in a bowl. Also chop the mange tous and rinse them briefly under cold water. Add them along with the chopped Chinese cabbage to the bowl of shiitake and soy shoots.",
+            "Remove the cooked rabbit legs from the broth and pour the broth through a sieve. Bring the broth to a boil again and add the sliced vegetables.",
+            "Remove the meat from the legs and add some more flavor by grating the garlic and ginger and adding a chopped chili pepper. Season with soy sauce and sesame oil. Scoop the meat into the jar of the blender and blend briefly.",
+            "Lay some of the wonton sheets open on your board or work surface. Place a mound of filling in the center of one sheet and fold the edges toward each other so that you have a nice filled triangle.",
+            "Bring some salted water to a boil and briefly cook the stuffed raviolis.",
+            "Add the coconut milk to the broth and stir well. Ladle some broth with cooked vegetables into a large bowl and spoon in some wonton raviolis. Top with some chopped spring onion."
+        ]
+    },
+    {
+        name: 'Belgian Pancakes',
+        prepTime: '20 minutes',
+        ingredients: ['2 eggs', '200g flour', '500ml milk', 'A pinch of salt'],
+        steps: [
+            'Add all the ingredients in a bowl and wisk them untill there are no clots left', 
+            'Heat the pan and add a little butter. Devide some dough in the pan and bake for 2-3 minutes until the top is dry, then flip the pancake and bake for 1 or 2 minutes more', 
+            'Put the pancakes on a plate and cover them. Then bake some more'
+        ]
+    },
+    {
+        name: "spaghetti",
+        prepTime: "35min",
+        ingredients: ["200g box of spaghetti", "500g tomato sauce", "250g of ground beef"],
+        steps: [
+            "Cook the meat and boil water", 
+            "Mix the sauce with the meat", 
+            "Cook the spaghetti 7 min", 
+            "Serve your saghetti"
+        ] 
+    },
+    {
+        name: 'Cake',
+        prepTime:'50 minutes',
+        ingredients: [ '3 cups of flour', '4eggs','2 teaspoon baking soda', '2 teaspoon vanilla essence', '1,5 cups of powdered sugar', '1 cup butter', '1 cup milk'],
+        steps:[
+            "Cream together butter-sugar and then blend with beaten eggs.", 
+            "Combine flour mix and beaten eggs", 
+            "Step 3 Bake the cake as per your convenience", 
+            "Step 4 Check with a knife or skewer if it is cooked and serve"
+        ]
+    },
+    {
+        name: "Macaroni and Cheese",
+        prepTime: "10min",
+        ingredients: ["200g box of spaghetti", "500g tomatosaus", "250g of ground beef"],
+        steps: [
+            "Boil macaroni", 
+            "Make a roux", 
+            "Add cheese and macaroni", 
+            "Serve your mac&cheese"
+        ]
+    }
+]
